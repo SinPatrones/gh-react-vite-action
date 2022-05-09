fac8f68d7f7526566e38dedb7e3e256734163dbc
Armando Hinojosa Cc <sinpatrones1@gmail.com>
2022-05-09 02:32:01 -0500

    FIX - Setting base URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://SinPatrones.github.io/',
+  base: 'https://sinpatrones.github.io/gh-react-vite-action/',
   plugins: [react()]
 })
